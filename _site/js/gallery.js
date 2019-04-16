eadab2cc324f43ca991f41ada9e36f1ec763c745
Dominik <dominik.alberski@gmail.com>
2019-04-16 08:21:16 +0200

    finished application

--- _site/js/gallery.js
+++ _site/js/gallery.js
@@ -1,3 +1,23 @@
 function activateGallery() {
-  alert("Gallery activated");
+  let thumbanails = document
+    .querySelector("#gallery-thumbs")
+    .querySelectorAll("img");
+    // querySelectorAll("#gallery-thumbs > div > img")
+  let mainImg = document.querySelector("#gallery-photo img")
+
+  let title = document.querySelector("#gallery-info .title")
+  let description = document.querySelector("#gallery-info .description")
+
+  thumbanails.forEach(function(thumbanail) {
+    thumbanail.addEventListener("click", function() {
+      mainImg.setAttribute("src", thumbanail.dataset.largeVersion);
+      mainImg.setAttribute("alt", thumbanail.alt);
+
+      title.textContent = thumbanail.dataset.title;
+      description.textContent = thumbanail.dataset.description;
+
+      document.querySelector('.current').classList.remove("current");
+      thumbanail.parentNode.classList.add("current");
+    })
+  })
 }
